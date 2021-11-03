ed026424eb94970715c6fcfea5f42d59e099b08c
周栒 <zhouxun@bonree.com>
2021-11-03 16:32:07 +0800

    修改api

--- API/baseapi.py
+++ API/baseapi.py
@@ -12,7 +12,8 @@ class BaseApi:
         :param kwargs: 其他参数
         :return:
         '''
-        r=requests.request(method, url, **kwargs)
+        conf=self.load_yaml('../config/baseconf.yml')
+        r=requests.request(method, conf['baseurl']+url, **kwargs)
         logger.info(f'******************接口请求信息********************\n{method, url, kwargs}')
         logger.info(f'******************接口返回信息********************\n{r.json()}')
         return r
--- API/test_1.py
+++ API/test_1.py
@@ -1,8 +1,8 @@
 from API.baseapi import BaseApi
 
-class Test1(BaseApi):
-    def test_1(self):
-        self.request('post',url='http://120.133.67.133:20007/rest/login',json={
+class TestDemo(BaseApi):
+    def test_demo(self):
+        self.request('post',url='/rest/login',json={
   "accountName": "admin",
   "password": "Vayt32IzfiAG/16UBOpEMaV9O6RxlZSZwxQuzRaj4q4=",
   "captcha": "bonr2ee"},
